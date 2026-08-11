7f45444b6aee679ed104e5404dc33d77f402e1bc
Ivan Korakin <57858990+murlockwarlock@users.noreply.github.com>
2026-08-12 03:44:20 +0500

    Исправить запуск Playwright в CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: 'tests/e2e',
+    reporter: process.env.CI ? [['line'], ['html', { open: 'never' }]] : 'list',
     webServer: {
         command: 'php artisan serve --host=127.0.0.1 --port=8000',
         url: 'http://127.0.0.1:8000/health',
