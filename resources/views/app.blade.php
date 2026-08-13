69fb4d7fa4c45f01ce9739f4c2c7fe9485409781
Ivan Korakin <57858990+murlockwarlock@users.noreply.github.com>
2026-08-14 01:22:22 +0500

    Add Chuklov branding assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="color-scheme" content="light">
+        <link rel="icon" type="image/png" href="{{ asset('brand/chuklov-mark.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('brand/chuklov-app-icon.png') }}">
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
         <x-inertia::head />
     </head>
---
+++ tests/Feature/BrandingAssetsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class BrandingAssetsTest extends TestCase
+{
+    public function test_original_brand_assets_are_preserved(): void
+    {
+        $assets = [
+            'brand/chuklov-emblem.png' => '724a54661c864b45531dc7855c7b354e82f49a69204ffff6b22eff6328a0e01b',
+            'brand/chuklov-mark.png' => 'a51aec77a667c92091ded3ac13d96cf4351a9a254d92ac14b411c80d6c88f973',
+            'brand/chuklov-app-icon.png' => 'efb403d1d2b1086e13abb928ed2a4ec20ac93a0bc95c682a3d00a59afc96a599',
+        ];
+
+        foreach ($assets as $path => $expectedHash) {
+            $absolutePath = public_path($path);
+
+            self::assertFileExists($absolutePath);
+            self::assertSame($expectedHash, hash_file('sha256', $absolutePath));
+        }
+    }
+
+    public function test_root_template_links_the_browser_icons(): void
+    {
+        $template = file_get_contents(resource_path('views/app.blade.php'));
+
+        self::assertIsString($template);
+        self::assertStringContainsString('rel="icon" type="image/png" href="{{ asset(\'brand/chuklov-mark.png\') }}"', $template);
+        self::assertStringContainsString('rel="apple-touch-icon" href="{{ asset(\'brand/chuklov-app-icon.png\') }}"', $template);
+    }
+}
