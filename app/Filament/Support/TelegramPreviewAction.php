2effb3ee66e8f30fdd341aaf1db91c80c2821d99
Ivan Korakin <57858990+murlockwarlock@users.noreply.github.com>
2026-09-07 03:08:33 +0500

    Use actual modal window geometry in acceptance flow

--- app/Filament/Support/TelegramPreviewAction.php
+++ app/Filament/Support/TelegramPreviewAction.php
@@ -18,7 +18,7 @@ final class TelegramPreviewAction
 {
     public static function make(Closure $messageBuilder): Action
     {
-        return FullRenderModalAction::make('telegramPreview')
+        return Action::make('telegramPreview')
             ->label('Предпросмотр Telegram')
             ->icon(Heroicon::OutlinedEye)
             ->modalHeading('Предпросмотр Telegram')
